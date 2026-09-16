620bf69a3d690517be26080ddcc06481321b02cc
Just1Fr <190003579+Just1Fr@users.noreply.github.com>
2026-09-16 18:06:20 +0200

    fix: correct parking duration and fare calculation

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,13 +10,11 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        double inHour = (double) ticket.getInTime().getTime() / 3600000;
+        double outHour = (double) ticket.getOutTime().getTime() / 3600000;
+        double duration = outHour - inHour;
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
-
-        switch (ticket.getParkingSpot().getParkingType()){
+        switch (ticket.getParkingSpot().getParkingType()) {
             case CAR: {
                 ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
                 break;
